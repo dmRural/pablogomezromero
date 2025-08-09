2207d4e255478d121983d49b950e2d4af16151a8
Adrián Gómez Llorente <adgllorente@gmail.com>
2025-08-09 13:18:05 +0200

    feat: Update SITE_DESCRIPTION and clean up MDX example content

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "pablogomezromero.com";
+export const SITE_DESCRIPTION =
+  "Conferenciante, experto en productividad y colaboración en equipo.";
