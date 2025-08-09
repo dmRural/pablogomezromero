984f30d243b05c42605e230c6cbdb78527570c71
Adrián Gómez Llorente <adgllorente@gmail.com>
2025-08-09 13:13:32 +0200

    feat: Add features collection and update index page to display features dynamically

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,18 +2,25 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
-      // Transform string to Date object
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       heroImage: image().optional(),
     }),
 });
 
-export const collections = { blog };
+const features = defineCollection({
+  loader: glob({ base: "./src/content/features", pattern: "**/*.{md,mdx}" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      icon: z.string(),
+    }),
+});
+
+export const collections = { blog, features };
