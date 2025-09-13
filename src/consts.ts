720ae0ecba42ba70105f82fdae0238af0d2a63e0
Adrián Gómez Llorente <adgllorente@gmail.com>
2025-09-13 13:54:31 +0200

    feat: actualizar descripciones en varias páginas para mejorar la claridad y el SEO

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,5 @@
 export const SITE_TITLE =
   "Asesor Financiero para Emprendedores | Pablo Gómez Romero";
 export const SITE_DESCRIPTION =
-  "Pablo Gómez Romero, asesor financiero especializado en consultoría para emprendedores, formación empresarial y desarrollo de negocios. Experto en finanzas, emprendimiento y productividad.";
+  "Pablo Gómez Romero, asesor financiero para emprendedores. Te ayudo a optimizar finanzas, planificar inversiones y desarrollar tu negocio. Consulta gratuita disponible.";
 export const WHATSAPP_PHONE_NUMBER = "34659291511";
