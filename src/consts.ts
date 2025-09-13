09c392736e8dc26366cdb0dfca507decc4caeef7
Adrián Gómez Llorente <adgllorente@gmail.com>
2025-09-13 13:49:41 +0200

    feat: actualizar metadatos y mejorar SEO en varias páginas, añadir StructuredData

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "pablogomezromero.com";
+export const SITE_TITLE =
+  "Pablo Gómez Romero - Asesor Financiero | Consultoría para Emprendedores";
 export const SITE_DESCRIPTION =
-  "Conferenciante, experto en productividad y colaboración en equipo.";
+  "Pablo Gómez Romero, asesor financiero especializado en consultoría para emprendedores, formación empresarial y desarrollo de negocios. Experto en finanzas, emprendimiento y productividad.";
 export const WHATSAPP_PHONE_NUMBER = "34659291511";
