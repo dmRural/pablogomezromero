b5ad8b43b39662abd15c356639e4f0a2b83a2a99
Adrián Gómez Llorente <adgllorente@gmail.com>
2025-09-13 13:52:48 +0200

    feat: actualizar títulos en varias páginas para mejorar la consistencia y el SEO

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "Pablo Gómez Romero - Asesor Financiero | Consultoría para Emprendedores";
+  "Asesor Financiero para Emprendedores | Pablo Gómez Romero";
 export const SITE_DESCRIPTION =
   "Pablo Gómez Romero, asesor financiero especializado en consultoría para emprendedores, formación empresarial y desarrollo de negocios. Experto en finanzas, emprendimiento y productividad.";
 export const WHATSAPP_PHONE_NUMBER = "34659291511";
